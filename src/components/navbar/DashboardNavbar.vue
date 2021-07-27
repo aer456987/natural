42cceaafd086f917fcbf368cd41e1da5c0fa02ce
Tori <aer456987@gmail.com>
2021-07-27 20:49:11 +0800

    連結 RWD 調整

--- src/components/navbar/DashboardNavbar.vue
+++ src/components/navbar/DashboardNavbar.vue
@@ -5,7 +5,7 @@
       <h1 class="m-0">
         <router-link
           to="/"
-          class="nav-link logo_brown h3 mb-0 p-0 border border-white"
+          class="nav-link logo_brown mb-0 p-0 border border-white"
         >
           Natural
         </router-link>
@@ -26,7 +26,7 @@
         id="navbarToggler"
         class="collapse navbar-collapse flex justify-content-end"
       >
-        <ul class="navbar-nav fs-5 text-center">
+        <ul class="navbar-nav navbar_text text-center">
           <li class="nav-item">
             <router-link
               to="/admin/coupon"
